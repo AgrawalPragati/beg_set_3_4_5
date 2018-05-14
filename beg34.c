c2e0b83ab1842334173145cf9ed9a8a830e49b58
AgrawalPragati <35077511+AgrawalPragati@users.noreply.github.com>
2018-05-14 21:44:55 +0530

    Add files via upload

---
+++ beg34.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[100]; int i=0,count=0,l;
+	gets(str);
+    while(str[i]!='\0')
+    {
+    	if(str[i]=='.')
+		{
+    	count++;
+		}
+		i++;
+	}
+	printf("%d",count);
+	return 0;
+}
